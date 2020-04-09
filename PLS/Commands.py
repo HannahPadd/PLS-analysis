8c3788e78c60a3c748e8f1ee44d2ff244c3d0d80
Lillie <57945088+LilliePad@users.noreply.github.com>
2020-04-09 22:27:56 +0200

    Started work on the commands handling

---
+++ PLS/Commands.py
@@ -0,0 +1,6 @@
+from book import book
+from user import User
+class Commands():
+
+    def command():
+        
--- PLS/PLS.py
+++ PLS/PLS.py
@@ -1,9 +1,12 @@
 from book import Book
 from customer import Customer
+from Commands import Commands
 import json
 
 programEnd = 0
 
 #the main loop for the program to run in.
 while(programEnd == 0):
-    #UwU
+    userInput = Command(input("Type your command:> "))
+    
+    
