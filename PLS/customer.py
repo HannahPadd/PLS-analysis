cf9f394bb4b0944de26318cf3e345117076c1606
0996736Ilias <0996736@hr.nl>
2020-05-10 18:17:26 +0200

    made user

--- PLS/customer.py
+++ PLS/customer.py
@@ -1,16 +1,25 @@
+import json
 class user:
-    def __init__ (self, userName, userID, userFirstName, userLastName):
+    def __init__ (self, userName, userID, userFirstName, userLastName, booksBorrowed):
         self.userName = userName
         self.userID = userID
         self.userFirstName = userFirstName
         self.userLastName = userLastName
+        self.booksBorrowed = booksBorrowed
 
     def CreateUser():
         
-        saveCustomer = {
+        saveUser = {
             "userName" : self.userName,
             "userLastName" : self.userLastName,
             "userFirstName" : self.userFirstName,
-            "userID" : self.userID
+            "userID" : self.userID,
+            "booksBorrowed" : self.booksBorrowed
         }
         json.dump(CreateUser)
+
+    def userList():
+        pass
+
+with open('users.txt', 'w') as outfile:
+    json.dump(CreateUser, outfile)
