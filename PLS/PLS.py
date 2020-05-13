06e01c8ef4dac7fda68152efd8041d177c827c7c
gioksz <57941142+gioksz@users.noreply.github.com>
2020-05-13 10:07:10 +0200

    starting the program
    starting the program

--- PLS/PLS.py
+++ PLS/PLS.py
@@ -1,4 +1,4 @@
-from Book import Book
+from book import Book
 from User import User
 from Commands import Commands
 
@@ -6,13 +6,27 @@ from database import Database
 import json
 
 programEnd = 0
+bookDatabase = open("bookDatabase.json", "w+")
+userDatabase = open("userDatabase.json", "w+")
 
 #the main loop for the program to run in.
 while(programEnd == 0):
-    bookDatabase = open("bookDatabase.json", "w+")
-    userDatabase = open("userDatabase.json", "w+")
-
-    Commands.userInput = input("type your command:> ")
+    userInput = input("""
+Press 1 to create an customer:
+Press 2 to create a book:
+Press 3 to Lend a book out:
+Press 4 to return a book: 
+Press 5 to Search for a book:""")
+    if userInput == 1:
+        print ("1")
+    if userInput == 2:
+        print("2")
+    if userInput == 3:
+        print("3")
+    if userInput ==4:
+        print("4")
+    if userInput == 5:
+        print("5")
     
     
 
