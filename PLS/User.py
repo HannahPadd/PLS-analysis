9bed0df1c38685294cc5f6f334102ce225b7589e
0996736Ilias <0996736@hr.nl>
2020-05-13 10:14:38 +0200

    user set correclty

--- PLS/User.py
+++ PLS/User.py
@@ -1,4 +1,26 @@
+import json
 class User:
-    def __init__(self, userName, userID):
+    def __init__ (self, userName, userID, userFirstName, userLastName):
         self.userName = userName
         self.userID = userID
+        self.userFirstName = userFirstName
+        self.userLastName = userLastName
+
+    def CreateUser(self):
+        
+        saveCustomer = {
+            "userName" : self.userName,
+            "userLastName" : self.userLastName,
+            "userFirstName" : self.userFirstName,
+            "userID" : self.userID
+        }
+        
+        saveUser = json.dumps(saveUser)
+        database = open("database.json", "w")
+        try:
+            database.write(saveUser)
+            database.close()
+            print("the user has been saved")
+        except:
+            database.close()
+            print("the user couldn't be saved")
