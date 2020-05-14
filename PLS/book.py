e3385a1c4fb5f8eb9bcbe47a22896db0f4af3bb1
Lillie <57945088+LilliePad@users.noreply.github.com>
2020-05-14 23:43:01 +0200

    Opzoeken van Boeken
    Ben bezig met het opzoeken van boeken te implementeren

--- PLS/book.py
+++ PLS/book.py
@@ -16,15 +16,46 @@ class Book:
         
         saveBook = json.dumps(saveBook) 
         database = open("bookDatabase.json", "r+")
-    #try:
-        data = json.loads(database)
-        data.update(saveBook)
-        data = json.dumps(data)
-        database.write(data)
-        database.close()
-        return "the book has been saved"
-    #except:
-        database.close()
-        return "the book couldn't be saved"
+        try:
+            data = json.loads(database)
+            data.update(saveBook)
+            data = json.dumps(data)
+            database.write(data)
+            database.close()
+            return "the book has been saved"
+        except:
+            database.close()
+            return "the book couldn't be saved"
     
-    
+    def lookupBook(self):
+        bookDatabase = open("booksset.json", "w")
+        bookDatabase = json.dumps(bookDatabase)
+        searchType = input("""do you want to search via:
+        - title
+        - author
+        - ISBN
+        Type the word for the search key
+        """)
+        searchType.lower()
+
+        if (searchType == "title"):
+            searchKey = input("Enter the title:> ")
+            for i in bookDatabase["title"]:
+                if (bookDatabase["title"] == searchKey):
+                    print("this book is at our library")
+                else:
+                    print("We do not have this book")
+
+        if (searchType == "author"):
+            searchKey = input("Enter the author:> ")
+            for i in bookDatabase["author"]:
+                if (bookDatabase["author"] == searchKey):
+                    print("I don't know what to do here")
+
+        if (searchType == "isbn"):
+            searchKey = input("Enter the ISBN:> ")
+            for i in bookDatabase["isbn"]:
+                if (bookDatabase["isbn"] == searchKey):
+                    print("This book is in our library")
+                else:
+                    print("We do not have this book in our library")
