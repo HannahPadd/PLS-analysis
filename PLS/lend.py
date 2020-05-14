ae8c63c869cb66d6c9e9b8cc7ff6e1073fc94c37
gioksz <57941142+gioksz@users.noreply.github.com>
2020-05-14 10:53:42 +0200

    added more commands and fixed isieu with lend

--- PLS/lend.py
+++ PLS/lend.py
@@ -2,13 +2,15 @@ import book
 import json
 
 class Lend():
-    def Lend():
+    def Lend(self):
         if bookAvailable == True:
             bookAvailable = False
             print("You are now borrowing this book")
         else: 
             print("Book is not avaliable")
-    def Return():
+
+            
+    def Return(self):
         if bookAvailable == False:
             bookAvailable = True
             print("You have now raturned this book")
